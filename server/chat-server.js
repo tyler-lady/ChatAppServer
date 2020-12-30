8de10c42242a0dcc1092a1a249f495f3d2e294a1
@tyler-lady <tclady53@gmail.com>
2020-12-30 12:29:07 -0500

    finished the thread loading capabilities. They now return with the user models attached in props as profiles. Those get passed to each individual message so the message has information about sender (Username/Avatar)

--- server/chat-server.js
+++ server/chat-server.js
@@ -4,7 +4,32 @@ var models = require('./server.js').models;
 const ws = new WebSocket.Server({port: 8080});
 const clients = []; //array to be used for listing all currently connected clients
 
+// const printClientCount = () => {
+//     console.log("Clients:", clients.length);
+// }
+
+//setInterval(printClientCount, 1000);
+
+//TODO: should add all of our error response messages, in case an error occurs during user exp I can then display it to the UI
+
 ws.on('connection', (ws) => {
+    function getInitialThreads(userId){
+        models.Thread.find({where: {}, include: 'Messages'}, (err, threads) => {
+            if(!err && threads){
+                threads.map((thread, i) => {
+                    models.User.find({where: {id: {inq: thread.users}}}, (err2, users) => {
+                        thread.profiles = users;
+                        if (i === threads.length - 1){
+                            ws.send(JSON.stringify({
+                                type: 'INITIAL_THREADS',
+                                data: threads
+                            }))
+                        }
+                    });
+                })
+            }
+        })
+    }
 
     function login(email, password){
         models.User.login({email:email, password:password}, (err, result) => {
@@ -21,6 +46,8 @@ ws.on('connection', (ws) => {
                             error: err2
                         }));  
                     } else {
+                        ws.uid = user.id + new Date().getTime().toString();
+
                         const userObject = { //object to rep a generic user
                             id: user.id,
                             email: user.email,
@@ -29,6 +56,8 @@ ws.on('connection', (ws) => {
 
                         clients.push(userObject); //upon successful login, object is created and added to clients array
                         console.log("Current Clients", clients);
+
+                        getInitialThreads(user.id);
 
                         ws.send(JSON.stringify({
                             type: 'LOGGEDIN',
@@ -42,6 +71,20 @@ ws.on('connection', (ws) => {
             }
         })
     }
+
+    ws.on('close', (req) => {
+        console.log('Request close', req);
+        let clientIndex = -1;
+        clients.map((c, i) => {
+            if(c.ws._closeCode === req){
+                clientIndex = i;
+            }
+        });
+
+        if(clientIndex > -1){
+            clients.splice(clientIndex, 1)
+        }
+    });
 
     ws.on('message', (message) => {
         console.log('Got Message', JSON.parse(message));
@@ -72,6 +115,31 @@ ws.on('connection', (ws) => {
                             })
                         }
                     });
+                    break;
+                case 'CONNECT_WITH_TOKEN':
+                    models.User.findById(parsed.data.userId, (err, user) => {
+                        if(!err && user){
+                            ws.uid = user.id + new Date().getTime().toString();
+                            const userObject = {
+                                id: user.id,
+                                email: user.email,
+                                ws: ws
+                            };
+    
+                            clients.push(userObject);
+                            console.log("Current Clients", clients);
+
+                            getInitialThreads(user.id);
+    
+                            // ws.send(JSON.stringify({
+                            //     type: 'LOGGEDIN',
+                            //     data: {
+                            //         session: result,
+                            //         user: user
+                            //     }
+                            // }))
+                        }
+                    })
                     break;
                 case 'LOGIN':
                     login(parsed.data.email, parsed.data.password);
@@ -118,6 +186,39 @@ ws.on('connection', (ws) => {
                                         type: 'ERROR',
                                         error: err2
                                     }));
+                                }
+                            })
+                        }
+                    })
+                    break;
+                case 'THREAD_LOAD':
+                    models.Message.find({where: {
+                        threadId: parsed.data.threadId,
+                    }, 
+                    order: 'date DESC',
+                    skip: parsed.data.skip,
+                    limit: 10}, (err, messages) => {
+                        if(!err && messages){
+                            ws.send(JSON.stringify({
+                                type: 'GOT_MESSAGES',
+                                threadId: parsed.data.threadId,
+                                messages: messages,
+                            }))
+                        }
+                    })
+                    break;
+                case 'ADD_MESSAGE':
+                    models.Thread.findById(parsed.threadId, (err, thread) => {
+                        if(!err && thread){
+                            models.Message.upsert(parsed.message, (err2, message) => {
+                                if(!err2 && message){
+                                    clients.filter(client => thread.users.indexOf(client.id.toString()) > -1).map(client => {
+                                        client.ws.send(JSON.stringify({
+                                            type: 'ADD_MESSAGE_TO_THREAD',
+                                            threadId: parsed.threadId,
+                                            message: message,
+                                        }))
+                                    })
                                 }
                             })
                         }
